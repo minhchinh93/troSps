e8cbe9bd31521a9314eef0c996ef73d27235446b
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-12-29 15:08:00 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('index');
 // });
-Route::get('/index', [troController::class, 'index'])->name('index');
+Route::get('/', [troController::class, 'index'])->name('index');
 // Route::get('/return', [troController::class])->name('return');
 // Route::post('/trosps', [troController::class, 'trosps'])->name('trosps');
 // dd('ok');
